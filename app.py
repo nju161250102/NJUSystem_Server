09aeec1409f86b52e44803dbd8ceaa428dbd0a7b
161250102 <161250102@smail.nju.edu.cn>
2018-07-19 15:37:26 +0800

    加入获取个人信息的接口

--- app.py
+++ app.py
@@ -1,12 +1,34 @@
+# -*- coding:utf-8 -*-
 from flask import Flask
 from flask import render_template
+from flask import request
+from login import loginModule
+import requests
+import json
 
 app = Flask(__name__)
+app.register_blueprint(loginModule, url_prefix='/api/login/')
 
 
 @app.route('/')
 def hello_world():
     return render_template('index.html')
+
+
+@app.route('/api/personInfo/', methods=['GET'])
+def person_info():
+    jar = requests.utils.cookiejar_from_dict(request.cookies)
+    r = requests.get("http://imp.nju.edu.cn/imp/iss/myAccount.do?action=getMyAcount", cookies=jar)
+    req_data = json.loads(r.content)
+    res_data = {
+        "name": req_data["username"],   # 姓名
+        "id": req_data["userid"],       # 学号
+        "identity": req_data["card"],   # 身份证号
+        "college": req_data["orgdn"],   # 所在学院
+        "phone": req_data["phone"],     # 联系电话
+        "email": req_data["email"]      # 电子邮箱
+    }
+    return json.dumps(res_data, ensure_ascii=False)
 
 
 if __name__ == '__main__':
